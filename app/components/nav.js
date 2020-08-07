2e3b1ebd72282005a02ecd62ca74f40e1f2a5950
Maleckim <maleckimatthewm@gmail.com>
2020-08-07 14:15:07 -0500

    dif style

--- app/components/nav.js
+++ app/components/nav.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function NavBar(props) {
   const [style, setStyle] = useState(false);
@@ -9,23 +11,21 @@ export default function NavBar(props) {
         <div className="p-2">
           <h1>Clipped+</h1>
         </div>
-        <div className="p-2 align-self-center">{props.children}</div>
-      </div>
-      <div className="row">
-        <div className="col w-100">
-          <div className={style ? 'search open w-100' : 'search'}>
-            <input type="text" className="search-box" />
-            <span
-              role="button"
-              tabIndex="0"
-              onKeyPress={() => setStyle(!style)}
-              className="search-button"
-              onClick={() => setStyle(!style)}
-            >
-              <span className="search-icon" />
-            </span>
+        <div className="p-2 w-50 align-self-center">
+          <div className="input-group navsrch">
+            <input
+              type="text"
+              placeholder="Search"
+              className="form-control navsrch"
+            />
+            <div className="input-group-append">
+              <button type="button" className="input-group-text navicn">
+                <FontAwesomeIcon icon={faSearch} className="navicn" />
+              </button>
+            </div>
           </div>
         </div>
+        <div className="p-2 align-self-center">{props.children}</div>
       </div>
     </div>
   );
--- app/global-styles.js
+++ app/global-styles.js
@@ -34,114 +34,18 @@ const GlobalStyle = createGlobalStyle`
     border-color: #00FEDE;
   }
 
-  .search {
-    transform:scale(0.6) translate(0, -40%);
-    width: 100px;
-    height: 100px;
-    margin: 40px auto 0;
-    background-color: #242628;
-    position: relative;
-    overflow: hidden;
-    transition: all .5s ease;
-    &:before {
-      content: '';
-      display: block;
-      width: 3px;
-      height: 100%;
-      position: relative;
-      background-color: #00FEDE;
-      transition: all .5s ease;
-    }
-    &.open {
-      width: 420px;
-      &:before {
-        height: 60px;
-        margin: 20px 0 20px 30px;
-        position: absolute;
-      }
-    }
+  .navsrch, .navsrch:focus {
+    border: 1px solid #00FEDE;
+    border-radius:6px;
+    background:transparent;
+    box-shadow:none;
+    color: white;
   }
   
-  .search-box {
-    width: 170%;
-    height: 100%;
-    box-shadow: none;
-    border: none;
-    background: #3a3d3b;
-    border-radius:8px;
-    color: #fff;
-    padding: 20px 100px 20px 45px;
-    font-size: 40px;
-    &:focus {
-      outline: none;
-    }
+  .navicn {
+    background: #242628;
+    color:#00FEDE;
   }
-  
-  .search-button {
-    outline-color: #242628;
-    width: 100px;
-    height: 100px;
-    display: block;
-    position: absolute;
-    right: 0;
-    top: 0;
-    padding: 20px;
-    cursor: pointer;
-  }
-  
-  .search-icon {
-    width: 40px;
-    height: 40px;
-    border-radius: 40px;
-    border: 3px solid #00FEDE;
-    display: block;
-    position: relative;
-    margin-left: 5px;
-    transition: all .5s ease;
-    &:before {
-      content: '';
-      width: 3px;
-      height: 15px;
-      position: absolute;
-      right: -2px;
-      top: 30px;
-      display: block;
-      background-color: #00FEDE;
-      transform: rotate(-45deg);
-      transition: all .5s ease;
-    }
-    &:after {
-      content: '';
-      width: 3px;
-      height: 15px;
-      position: absolute;
-      right: -12px;
-      top: 40px;
-      display: block;
-      background-color: #00FEDE;
-      transform: rotate(-45deg);
-      transition: all .5s ease;
-    }
-    .open & {
-      margin: 0;
-      width: 60px;
-      height: 60px;
-      border-radius: 60px;
-      &:before {
-        transform: rotate(52deg);
-        right: 22px;
-        top: 23px;
-        height: 18px;
-      }
-      &:after {
-        transform: rotate(-230deg);
-        right: 22px;
-        top: 13px;
-        height: 18px;
-      }
-    }
-  }
-  
 
   
 `;
